24444a74a8a2be0bf85e805c7a5bf7edca2ce56d
Kylie Le <kylie.tram.le@gmail.com>
2016-09-10 14:01:33 -0700

    table view and detail view controller

---
+++ NowPlaying/MovieCell.swift
@@ -0,0 +1,22 @@
+//
+//  MovieCell.swift
+//  NowPlaying
+//
+//  Created by Kylie Tram Le on 9/10/16.
+//  Copyright © 2016 Kylie Tram Le. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    
+    @IBOutlet weak var moviePosterImageView: UIImageView!
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- NowPlaying/ViewController.swift
+++ NowPlaying/ViewController.swift
@@ -8,18 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
-
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
+        cell.moviePosterImageView.image = UIImage(named: "suicidesquad")
+        cell.movieTitleLabel.text = "Suicide Squad"
+        
+        return cell
+    }
 
 }
 
